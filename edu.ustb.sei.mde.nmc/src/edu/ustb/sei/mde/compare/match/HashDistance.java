720e194d80eab1e77ddc708862a339718d9a6c26
Joseph <2449660411@qq.com>
2021-11-18 21:29:40 +0800

    add HashDistance (to be implement)

---
+++ edu.ustb.sei.mde.nmc/src/edu/ustb/sei/mde/compare/match/HashDistance.java
@@ -0,0 +1,22 @@
+package edu.ustb.sei.mde.compare.match;
+
+import org.eclipse.emf.ecore.EObject;
+
+import edu.ustb.sei.mde.compare.Comparison;
+import edu.ustb.sei.mde.compare.HashFunction;
+
+public class HashDistance implements HashFunction {
+
+	@Override
+	public double distance(Comparison inProgress, EObject a, EObject b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean areIdentic(Comparison inProgress, EObject a, EObject b) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
